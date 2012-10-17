8a69b3f8ab6090c0a6f5a4e2599e6e05bc6857ad
David Luecke <daff@neyeon.com>
2012-10-17 16:22:13 -0600

    Cleanup

--- index.js
+++
@@ -1,8 +0,0 @@
-/*
-* Index file to include the Caress Server
-*
-* Copyright (c) 2012 Eric Kryski
-* MIT Licensed
-*/
-
-module.exports = require("./lib/caress-server");
---
+++ test/tuio-packet.js
@@ -0,0 +1,7 @@
+var expect = require('chai').expect;
+
+describe("TUIO packet", function() {
+	it("Runs a test!", function() {
+		expect(2).to.equal(2);
+	})
+});
